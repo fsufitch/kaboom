0c0cd03f70ef1ce8ee9107ca3480b579411625f2
Filip Sufitchi <fsufitchi@gmail.com>
2025-12-31 02:00:46 +0000

    Improve bazelrc gen script

--- .devcontainer/init-bazel.sh
+++ .devcontainer/init-bazel.sh
@@ -5,6 +5,7 @@ if [[ -z "${REPO_DIR}" ]]; then
   echo "Usage: $0 <path-to-repo>"
   exit 1
 fi
+REPO_DIR="$(cd "${REPO_DIR}" && pwd)"
 cd "${REPO_DIR}"
 
 # Create .bazelrc.local with devcontainer-specific Bazel settings.
@@ -13,11 +14,16 @@ if [[ -f .bazelrc.local ]]; then
 else
   echo "Generating .bazelrc.local with devcontainer-specific settings."
   cat > .bazelrc.local <<EOF
-  # Use mounted space for Bazel cache and repository cache, to avoid overlayfs performance issues.
-startup --output_user_root=$(pwd)/.bazel_cache
-common --repository_cache=$(pwd)/.bazel_repo_cache
+# Use mounted space for Bazel cache and repository cache, to avoid overlayfs performance issues.
+startup --output_user_root=${REPO_DIR}/.bazel_cache
+common --repository_cache=${REPO_DIR}/.bazel_repo_cache
 EOF
 fi
 
 # Generate compile_commands.json for clangd and clang-tidy.
-bazel run //:compdb
+if [[ -f compile_commands.json ]]; then
+  echo "compile_commands.json already exists; skipping generation."
+else
+  echo "Generating compile_commands.json using Bazel."
+  bazel run //:compdb
+fi
