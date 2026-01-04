9c607c3f6e8195ac592ab776d269a4d4efe6bc52
Filip Sufitchi <fsufitchi@gmail.com>
2026-01-04 19:57:04 +0000

    Blank slate for TS rewrite

--- kaboomstate/errors.go
+++
@@ -1,9 +0,0 @@
-package kaboomstate
-
-import "errors"
-
-var (
-	ErrInvalidProto        = errors.New("invalid proto data")
-	ErrMoveHasNoTrajectory = errors.New("move has no board trajectory")
-	ErrMoveMissingPosition = errors.New("move missing position data")
-)
