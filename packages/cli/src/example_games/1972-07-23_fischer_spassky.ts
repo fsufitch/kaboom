b99a9c904dae3af7fe5cd5ff779d27ecdf28b7a2
Filip Sufitchi <fsufitchi@gmail.com>
2026-01-07 05:44:02 +0000

    Add example game and replays

--- packages/cli/src/example_games/1972-07-23_fischer_spassky.ts
+++ packages/cli/src/example_games/1972-07-23_fischer_spassky.ts
@@ -1,9 +1,11 @@
 import { KaboomEngine } from '@kaboom/engine';
-import { ClassicChessRuleset, newClassicChessGame } from '@kaboom/engine/classic';
+import { ClassicChessRuleset } from '@kaboom/engine/classic';
 import { IntendedTurn } from '@kaboom/proto';
 
+import { createClassicMoveBuilders } from './helpers';
+
 const engine = new KaboomEngine(ClassicChessRuleset);
-let game = newClassicChessGame({
+let game = ClassicChessRuleset.newGame({
   gameId: '1972-07-23_fischer_spassky',
   whitePlayerId: 'white',
   whitePlayerName: 'Robert James Fischer',
@@ -15,67 +17,22 @@ const boardId = game.boards[0]!.id;
 const whiteId = game.players[0]!.id;
 const blackId = game.players[1]!.id;
 
-type File = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
-const fileToColumn: Record<File, number> = {
-  a: 0,
-  b: 1,
-  c: 2,
-  d: 3,
-  e: 4,
-  f: 5,
-  g: 6,
-  h: 7,
-};
-
-const square = (file: File, rank: number) => ({
-  boardId,
-  boardPosition: { row: rank - 1, column: fileToColumn[file] },
-});
-
-const pawnOneStep = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { pawn: { oneStep: { from, to } } },
-});
-const pawnTwoStep = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { pawn: { twoStep: { from, to } } },
-});
-const pawnCapture = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { pawn: { capture: { from, to } } },
-});
-const rookMove = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { rook: { move: { from, to } } },
-});
-const rookCapture = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { rook: { capture: { from, to } } },
-});
-const knightMove = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { knight: { move: { from, to } } },
-});
-const knightCapture = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { knight: { capture: { from, to } } },
-});
-const bishopMove = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { bishop: { move: { from, to } } },
-});
-const bishopCapture = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { bishop: { capture: { from, to } } },
-});
-const queenMove = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { queen: { move: { from, to } } },
-});
-const queenCapture = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { queen: { capture: { from, to } } },
-});
-const kingMove = (from: ReturnType<typeof square>, to: ReturnType<typeof square>) => ({
-  classicMove: { king: { move: { from, to } } },
-});
-const kingCastle = (
-  kingFrom: ReturnType<typeof square>,
-  kingTo: ReturnType<typeof square>,
-  rookFrom: ReturnType<typeof square>,
-  rookTo: ReturnType<typeof square>,
-) => ({
-  classicMove: { king: { castle: { kingFrom, kingTo, rookFrom, rookTo } } },
-});
+const {
+  square,
+  pawnOneStep,
+  pawnTwoStep,
+  pawnCapture,
+  rookMove,
+  rookCapture,
+  knightMove,
+  knightCapture,
+  bishopMove,
+  bishopCapture,
+  queenMove,
+  queenCapture,
+  kingMove,
+  kingCastle,
+} = createClassicMoveBuilders(boardId);
 
 let turnId = 1;
 const play = (playerId: string, moves: IntendedTurn['moves']) => {
@@ -98,9 +55,7 @@ play(blackId, [knightMove(square('g', 8), square('f', 6))]);
 play(whiteId, [knightMove(square('b', 1), square('c', 3))]);
 play(blackId, [bishopMove(square('f', 8), square('e', 7))]);
 play(whiteId, [bishopMove(square('c', 1), square('g', 5))]);
-play(blackId, [
-  kingCastle(square('e', 8), square('g', 8), square('h', 8), square('f', 8)),
-]);
+play(blackId, [kingCastle(square('e', 8), square('g', 8), square('h', 8), square('f', 8))]);
 play(whiteId, [pawnOneStep(square('e', 2), square('e', 3))]);
 play(blackId, [pawnOneStep(square('h', 7), square('h', 6))]);
 play(whiteId, [bishopMove(square('g', 5), square('h', 4))]);
@@ -121,9 +76,7 @@ play(whiteId, [bishopMove(square('f', 1), square('b', 5))]);
 play(blackId, [pawnOneStep(square('a', 7), square('a', 6))]);
 play(whiteId, [pawnCapture(square('d', 4), square('c', 5))]);
 play(blackId, [pawnCapture(square('b', 6), square('c', 5))]);
-play(whiteId, [
-  kingCastle(square('e', 1), square('g', 1), square('h', 1), square('f', 1)),
-]);
+play(whiteId, [kingCastle(square('e', 1), square('g', 1), square('h', 1), square('f', 1))]);
 play(blackId, [rookMove(square('a', 8), square('a', 7))]);
 play(whiteId, [bishopMove(square('b', 5), square('e', 2))]);
 play(blackId, [knightMove(square('b', 8), square('d', 7))]);
